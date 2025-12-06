015be21eeacc1d9bb3575f42516bf42e27434c58
Conner Olsen <contact@conner-olsen.dev>
2025-12-06 15:05:45 -0500

    Update prepare-release.sh

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-rsync -av --inplace --no-times in_game/ main_menu/ dist/
+rsync -av --inplace --no-times in_game main_menu dist/
 
 rm -rf ../improved-subject-management
 cp -r dist ../improved-subject-management
