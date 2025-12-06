726a8fb8d8b2af28777c539e43f7c661384e0e09
Conner Olsen <contact@conner-olsen.dev>
2025-12-06 14:56:05 -0500

    Update prepare-release.sh

--- prepare-release.sh
+++ prepare-release.sh
@@ -3,7 +3,7 @@ set -e
 
 rsync -av --inplace --no-times in_game/ main_menu/ dist/
 
-rm -rf ../fix-trade-companies
-cp -r dist ../fix-trade-companies
+rm -rf ../improved-subject-management
+cp -r dist ../improved-subject-management
 
 find dist -mindepth 1 -maxdepth 1 ! -name '.metadata' -exec rm -rf {} +
